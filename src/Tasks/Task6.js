1574de75a47419ca0ecaa1eed8c8227cd2b800b5
Q-Digital <no-reply@q-digital.org>
2022-04-04 22:50:39 +0400

    Add 6 task

--- src/Tasks/Task6.js
+++ src/Tasks/Task6.js
@@ -2,17 +2,16 @@ import React, { useState } from "react";
 import './styles.css'
 
 export const Task6 = (props) => {
-    const [string, setString] = useState('')
-    const [numbers, setNumbers] = useState([])
+    const [count0, setCount0] = useState([])
+    const [count1, setCount1] = useState([])
 
     const findNumbers = (e) => {
         let str = e.target.value
-        // setNumbers(str)
-        let arr = []
-        
-        console.log(str.split(''));
+        let count1 = str.split("0").filter(e => e.length != 0 && (e.length + 1) % 2)
+        let count0 = str.split("1").filter(e => e.length != 0 && (e.length + 1) % 2)
+        setCount0(count0)
+        setCount1(count1)
     }
-
     return (
         <div className='center '>
             <h1>Task6</h1>
@@ -21,8 +20,11 @@ export const Task6 = (props) => {
                     <input style={{ width: 400, height: 50 }} placeholder='Введите строку' onChange={findNumbers} ></input>
                 </div>
             </div>
-            <div>Числа в строке {numbers.map((i, index) => (
-                <span style={{ marginRight: 5 }} key={index}>{i}</span>
+            <div>{count0.map((i, index) => (
+                <span style={{ marginRight: 5 }} key={index}>{'[' + i + ']'}</span>
+            ))}</div>
+            <div>{count1.map((i, index) => (
+                <span style={{ marginRight: 5 }} key={index}>{'[' + i + ']'}</span>
             ))}</div>
         </div>
     )
